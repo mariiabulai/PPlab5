0d50fc455d733026daae5dd2323bbe99d9b3f814
mariiabulai <282655@student.pwr.edu.pl>
2026-05-28 16:08:54 +0200

    Naprawienie błędów

--- utils.py
+++ utils.py
@@ -1,17 +1,23 @@
-# Exemplary calculator functions
+"""
+Exemplary calculator functions
+"""
 
 
 def add(a: int, b: int) -> int:
+    """Adds two numbers a and b."""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Subtracts number b from a."""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """Multiplies two numbers a and b."""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """Divides number a by b."""
     return a / b
