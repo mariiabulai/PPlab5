1c80651d25668102fc76622ffedcf3e68e262a7b
mariiabulai <282655@student.pwr.edu.pl>
2026-05-28 16:14:36 +0200

    Naprawienie błędów

--- test_utils.py
+++ test_utils.py
@@ -1,9 +1,12 @@
+"""Test module for utils module functions"""
+
 import pytest
 import utils
 
 
 @pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (2, 3, 5), (3, 4, 7), (4, 5, 9)])
 def test_add(a, b, expected):
+    """Test the add function"""
     result = utils.add(a, b)
     assert result == expected
 
@@ -12,6 +15,7 @@ def test_add(a, b, expected):
     "a, b, expected", [(1, 2, -1), (2, 3, -1), (3, 4, 1), (4, 5, -1)]
 )
 def test_subtract(a, b, expected):
+    """Test the subtract function"""
     result = utils.subtract(a, b)
     assert result == expected
 
@@ -20,11 +24,13 @@ def test_subtract(a, b, expected):
     "a, b, expected", [(1, 2, 2), (2, 3, 6), (3, 4, 12), (4, 5, 20)]
 )
 def test_multiply(a, b, expected):
+    """Test the multiply function"""
     result = utils.multiply(a, b)
     assert result == expected
 
 
 @pytest.mark.parametrize("a, b, expected", [(1, 2, 0.5), (3, 4, 0.75), (4, 5, 0.8)])
 def test_divide(a, b, expected):
+    """Test the divide function"""
     result = utils.divide(a, b)
     assert result == expected
