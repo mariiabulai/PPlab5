57ab094b608b067566f750b333d9adf23a2cd3a4
mariiabulai <282655@student.pwr.edu.pl>
2026-05-28 16:26:17 +0200

    Nowa funkcja

--- utils.py
+++ utils.py
@@ -21,3 +21,12 @@ def multiply(a: int, b: int) -> int:
 def divide(a: int, b: int) -> float:
     """Divides number a by b."""
     return a / b
+
+
+def decimal_to_binary(n: int) -> str:
+    """Converts a decimal number to binary string."""
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("Liczba musi być naturalna")
+    if n > 100:
+        raise ValueError("Liczba musi być z zakresu od 0 do 100")
+    return bin(n)[2:]
